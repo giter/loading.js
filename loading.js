afd2c4ef86bf77f395e05c0e31199f1ec7a9dddf
jjli <jjli@jjli-virt.(none)>
2013-12-05 14:37:32 +0800

    Format code without <Tab>

--- loading.js
+++ loading.js
@@ -5,116 +5,116 @@
 
 $.extend({ loading: function(){
 
-		if(arguments.length == 0){
+    if(arguments.length == 0){
 
 
-			$(".position").each(function(){
+      $(".position").each(function(){
 
-				var position = $(this).attr("id");
-				if(!!!position) return;
-			
+        var position = $(this).attr("id");
+        if(!!!position) return;
+      
 
-				$(".section").each(function(){
-					
-					var section = $(".section").attr("class").match(/section-([^\s]+)/)[1] || undefined;
-					if(!!!section) return;
+        $(".section").each(function(){
+          
+          var section = $(".section").attr("class").match(/section-([^\s]+)/)[1] || undefined;
+          if(!!!section) return;
 
-					call(position,section,$(this));
-				});
-			});
+          call(position,section,$(this));
+        });
+      });
 
-			return;
-		}
+      return;
+    }
 
 
 
-		url = arguments[0] || undefined;
-		opts = arguments[1] || undefined;
+    url = arguments[0] || undefined;
+    opts = arguments[1] || undefined;
 
-		opts = $.extend({
-			method: "get",
-			cache: false,
-			data: {} ,
-			dataType: "json"
-		},opts);
+    opts = $.extend({
+      method: "get",
+      cache: false,
+      data: {} ,
+      dataType: "json"
+    },opts);
 
-		$.ajax({
+    $.ajax({
 
-			url:url,
-			type:opts.method,
-			data:(opts.data),
+      url:url,
+      type:opts.method,
+      data:(opts.data),
 
-			success:function(sections){
+      success:function(sections){
 
-				for(var i in sections){
+        for(var i in sections){
 
-					sec = sections[i];
+          sec = sections[i];
 
-					if($("#"+sec.position).size() > 0) {
+          if($("#"+sec.position).size() > 0) {
 
-						var h = "#"+sec.position + " .section-"+sec.section;
+            var h = "#"+sec.position + " .section-"+sec.section;
 
-						if($(h).size()>0){
-							$(h).first().each(function(){
-								this.innerHTML = (sec.data);
-							});
-						}else{
-							var o = $("<div class='section section-"+sec.section+"'>"+"</div>");
-							o[0].innerHTML = (sec.data);
-							$("#"+sec.position).prepend(o);
-						}
+            if($(h).size()>0){
+              $(h).first().each(function(){
+                this.innerHTML = (sec.data);
+              });
+            }else{
+              var o = $("<div class='section section-"+sec.section+"'>"+"</div>");
+              o[0].innerHTML = (sec.data);
+              $("#"+sec.position).prepend(o);
+            }
 
-						call(sec.position,sec.section,$(h));
-					}
-				}
+            call(sec.position,sec.section,$(h));
+          }
+        }
 
-				opts.success && opts.success();
-				var hash = url.match(/#[a-zA-Z0-9\-]+/);
-				if(hash = hash && hash[0]){
-					hash = hash.replace(/\?.*$/,"");
-					if($(hash).length > 0){
-						$("html,body").animate({ scrollTop: $(hash).offset().top},"fast");
-					}
-				}
-			},
-			error:function(xhr, status, err){
-				opts.error && opts.error();
-			}
-		});
-	}
+        opts.success && opts.success();
+        var hash = url.match(/#[a-zA-Z0-9\-]+/);
+        if(hash = hash && hash[0]){
+          hash = hash.replace(/\?.*$/,"");
+          if($(hash).length > 0){
+            $("html,body").animate({ scrollTop: $(hash).offset().top},"fast");
+          }
+        }
+      },
+      error:function(xhr, status, err){
+        opts.error && opts.error();
+      }
+    });
+  }
 });
 
 __loading_callbacks = {};
 
 function bind(pos,id,func){
-	if(!__loading_callbacks[pos]){
-		__loading_callbacks[pos] = {};
-	}
-	__loading_callbacks[pos][id] = func;
+  if(!__loading_callbacks[pos]){
+    __loading_callbacks[pos] = {};
+  }
+  __loading_callbacks[pos][id] = func;
 }
 
 function call(pos,id,target){
-	if(pos && id && __loading_callbacks[pos] && __loading_callbacks[pos][id]){
-		__loading_callbacks[pos][id](target);
-	}
+  if(pos && id && __loading_callbacks[pos] && __loading_callbacks[pos][id]){
+    __loading_callbacks[pos][id](target);
+  }
 }
 
 $(function(){
 
-	$("body").delegate(".loading" ,"click",function(){
+  $("body").delegate(".loading" ,"click",function(){
 
-		if($(this).is("form")) return true;
-		
-		if(!!!$(this).attr("uri")) $(this).attr("uri",$(this).attr("href"));
+    if($(this).is("form")) return true;
+    
+    if(!!!$(this).attr("uri")) $(this).attr("uri",$(this).attr("href"));
 
-		$(this).attr("uri") && $.loading($(this).attr("uri"),{method:$(this).attr("method")});
-		return false;
-	});
+    $(this).attr("uri") && $.loading($(this).attr("uri"),{method:$(this).attr("method")});
+    return false;
+  });
 
-	$("body").delegate("form.loading" ,"submit",function(e){
+  $("body").delegate("form.loading" ,"submit",function(e){
 
-		var p = ($(this).attr("action")||"").match(/([^#]*)?(#.*)?$/)
-		$.loading((p[1]||"") + (p[2]||""),{method:$(this).attr("method"),data:$(this).serialize()});
-		return false;
-	});
+    var p = ($(this).attr("action")||"").match(/([^#]*)?(#.*)?$/)
+    $.loading((p[1]||"") + (p[2]||""),{method:$(this).attr("method"),data:$(this).serialize()});
+    return false;
+  });
 });
